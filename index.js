deb1c8c97a81428e4a50eaa4a0809c148103411e
Maggie-karuri <karurimargaret111@gmail.com>
2024-03-26 22:50:47 +0300

    match drivers

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+console.log(drivers);
+console.log(findMatching);
+console.log(fuzzyMatch);
+console.log(matchName);
+
